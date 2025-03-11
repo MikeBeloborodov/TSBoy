1e6ab4e98460006af2d57f93324fbf994153cfad
testforregistration123 <mike.beloborodov.work@gmail.com>
2025-03-11 20:09:03 +0400

    init
    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+// eslint.config.js
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
+
+export default [
+  {
+    files: ['**/*.ts'], // Apply to all TypeScript files
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+];
